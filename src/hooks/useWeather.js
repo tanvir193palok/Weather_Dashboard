f174bd70f1d8e2f9651fa6b979c8247c68657d79
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-02-10 08:23:09 +0600

    Implemented a custom hook to fetch data from openWeather API

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,76 @@
+const { useState } = require("react");
+
+const useWeather = () => {
+  //data state
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemparature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+
+  //loading state
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+
+  //error state
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching Weather Data",
+      });
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units=metric`
+      );
+
+      if (!response.ok) {
+        const errorMessage = `An error occured during fetchinng data : ${response.status}`;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+
+      const updatedWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        maxTemparature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude: longitude,
+        latitude: latitude,
+      };
+
+      setWeatherData(updatedWeatherData);
+      
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: "",
+      });
+    }
+  };
+};
