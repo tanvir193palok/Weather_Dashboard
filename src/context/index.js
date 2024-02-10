ca9e478c4ded37e60783cdc744868c99534285b2
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-02-11 05:26:04 +0600

    Added a favourite provider with toggle favourite functionality

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
 const WeatherContext = createContext("");
+const FavouriteContext = createContext("");
 
-
-export {WeatherContext}
+export { WeatherContext, FavouriteContext };
---
+++ src/provider/FavouriteProvider.jsx
@@ -0,0 +1,32 @@
+import { FavouriteContext } from "../context";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const FavouriteProvider = ({ children }) => {
+  const [favourites, setFavourites] = useLocalStorage("favourites", []);
+
+  const addToFavourites = (latitude, longitude, location) => {
+    setFavourites(...favourites, {
+      latitude: latitude,
+      longitude: longitude,
+      location: location,
+    });
+  };
+
+  const removeFromFavourites = (location) => {
+    const restFavourites = favourites.filter(
+      (fav) => fav.location !== location
+    );
+
+    setFavourites(restFavourites);
+  };
+
+  return (
+    <FavouriteContext.Provider
+      value={{ favourites, addToFavourites, removeFromFavourites }}
+    >
+      {children}
+    </FavouriteContext.Provider>
+  );
+};
+
+export default FavouriteProvider;
